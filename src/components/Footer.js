906301d9d82a11c8b75bbe71a8ac3d9cb48d4cdd
Marcelo Espinoza <marcelo>
2024-04-25 18:30:47 -0300

    se agrega social icons al footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillInstagram,
+  AiFillFacebook 
+} from "react-icons/ai";
 
 function Footer() {
   let date = new Date();
@@ -7,9 +11,34 @@ function Footer() {
   
   return (
     <Container fluid className="footer">
-      <Row>
-        <Col md="4" className="footer-copywright">
-          <h3>Idea y desarrollo: Marcelo Espinoza</h3>
+      <Row>       
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="footer-copywright social-icons">
+            <h3>Idea y desarrollo: Marcelo Espinoza</h3>
+
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.facebook.com/groups/791116471254328/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillFacebook />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/soumyajit4419"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Formosa - Argentina</h3>
